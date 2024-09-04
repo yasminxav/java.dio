74cd82aa3a91a6ad83c153a3f0f0fca573d9468c
Yasmin Gabriele Xavier <ygxavier.yx@gmail.com>
2024-09-03 21:26:56 -0300

    Novas funcionalidades

---
+++ Anatomia das cLASSES/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Yasmin";
+        String segundoNome = "Gabriele";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+    
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
