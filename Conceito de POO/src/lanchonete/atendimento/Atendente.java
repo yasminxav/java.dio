3b160e2f0e15a680488f30c9496fb20defb6534e
Yasmin Gabriele Xavier <ygxavier.yx@gmail.com>
2024-10-30 20:20:10 -0300

    visibilidade de recursos

---
+++ Conceito de POO/src/lanchonete/atendimento/Atendente.java
@@ -0,0 +1,19 @@
+package lanchonete.atendimento;
+
+public class Atendente {
+    public void servindoMesa() {
+        System.out.println("SERVINDO A MESA");
+    };
+    public void pegarLancheCozinha () {
+        System.out.println("PEGANDO O LANCHE COZINHA");
+    };
+    public void receberPagamento () {
+        System.out.println("RECEBENDO PAGAMENTO");
+    };
+    public void trocarGas() {
+        System.out.println("ATENDENTE TROCANDO O GAS");
+    };
+    public void pegarPedidoBalcao () {
+        System.out.println("PEGANDO O PEDIDO NO BALCAO");
+    }
+}
---
+++ Conceito de POO/src/lanchonete/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,16 @@
+package lanchonete.atendimento.cozinha;
+
+public class Almoxarife {
+    public void controlarEntrada() {
+        System.out.println("CONTROLANDO ENTRADA");
+    };
+    public void controlarSaida () {
+        System.out.println("CONTROLAR SAIDA"); 
+    };
+    public void entregarIngredientes () {
+        System.out.println("ENTREGANDO INGREDIENTES");
+    };
+    public void trocarGas () {
+        System.out.println("ALMOXARIFE TROCANDO O GAS");
+    }
+}
