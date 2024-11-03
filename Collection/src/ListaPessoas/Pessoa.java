5a4dd070b9402451502dd835994bed763cf0f5bf
Yasmin Gabriele Xavier <ygxavier.yx@gmail.com>
2024-11-03 19:23:51 -0300

    Métodos de Ordenação

---
+++ Collection/src/ListaPessoas/Pessoa.java
@@ -0,0 +1,29 @@
+package ListaPessoas;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa (String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;       
+    };
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public String toString() {
+        return "Pessoa {" + 
+        "nome= '" + nome + "\'" +
+        ", idade= '" + idade + "\'" +
+        ", altura= " + altura +
+        "}";
+    }
+}
